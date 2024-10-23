89b7b646bd6cdc489f1c3c01e1e6d8b0727e680b
Membrillo Gaitan Jorge <>
2024-10-23 18:19:20 -0500

    hola soy sandro

--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -15,6 +15,8 @@ public class Test2 {
      */
     public static void main(String[] args) {
         System.out.println("HOLAAAA");
+        
+        System.out.println("ADIOSSS");
     }
     
 }
